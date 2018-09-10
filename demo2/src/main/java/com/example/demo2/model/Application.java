b13bc919d88c99114c292d0a5aa2f958ff5ec243
Kamala <hemalathapindi@gmail.com>
2018-09-10 23:29:53 +0530

    123

---
+++ demo2/src/main/java/com/example/demo2/model/Application.java
@@ -0,0 +1,19 @@
+
+
+package com.example.demo2.model;
+
+public class Application {
+	ChartContainer ChartContainerObject;
+
+	// Getter Methods
+
+	public ChartContainer getChartContainer() {
+		return ChartContainerObject;
+	}
+
+	// Setter Methods
+
+	public void setChartContainer(ChartContainer chartContainerObject) {
+		this.ChartContainerObject = chartContainerObject;
+	}
+}
---
+++ demo2/src/main/java/com/example/demo2/model/datapoints.java
@@ -0,0 +1,22 @@
+package com.example.demo2.model;
+
+
+public class datapoints {
+
+	private int Y;
+	private String Name;
+	public int getY() {
+		return Y;
+	}
+	public void setY(int y) {
+		Y = y;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	
+	
+}
